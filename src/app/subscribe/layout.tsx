2da7811283ac1d119c9d3026ed8e361f90758632
Hashem Productions <alloussalim@gmail.com>
2025-09-04 00:14:19 +0200

    chore: drop segment layouts and rely on root layout

--- src/app/subscribe/layout.tsx
+++
@@ -1,8 +0,0 @@
-import type { ReactNode } from "react";
-
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
-
-export default function SubscribeLayout({ children }: { children: ReactNode }) {
-  return <>{children}</>;
-}
